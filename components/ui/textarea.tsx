c01112deeb13ae7c86567a37c669bf588ad9be46
SAKA-LG <kadekisaka00@gmail.com>
2025-08-09 17:05:31 +0800

    remove bug

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -7,7 +7,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(({ classNa
   return (
     <textarea
       className={cn(
-        "w-full rounded-lg border border-input bg-background px-3 py-2 text-black dark:text-white placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+        "w-full rounded-lg border border-input bg-background px-3 py-2 text-black  placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
         className,
       )}
       ref={ref}
