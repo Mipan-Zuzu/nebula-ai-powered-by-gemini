5f0b5a10799eb11cd605a8012f9b56352f311b48
MIPAN <kadekisaka00@gmail.com>
2025-08-09 18:20:42 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: 'Nebula',
 }
 
 export default function RootLayout({
